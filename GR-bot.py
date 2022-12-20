22ba14f82ff953989d39e26d768a0cca5f2824d3
Cesar Santos <C3S4R780@gmail.com>
2022-12-20 16:01:33 -0300

    [Add] Bot activity

--- GR-bot.py
+++ GR-bot.py
@@ -1,25 +1,24 @@
 # Imports
 import nextcord
 from nextcord.ext import commands
-import wavelink
 import os
 from apiKeys import BOTTOKEN
 
 # Global variables
 intents = nextcord.Intents.all() # Setup bot intents
-client = commands.Bot(command_prefix="/", intents=intents) # Create bot client
+activity = nextcord.Activity(type=nextcord.ActivityType.listening, name='Rap de anime') # Bot activity
+client = commands.Bot(command_prefix="/", intents=intents, activity=activity) # Create bot client
 
 # --- Events ---
 # @client.event
 # async def on_ready():
 #     print("--- Ready ---")
 
+# Cogs imports
 initial_extensions = []
-
 for filename in os.listdir("./cogs"):
     if (filename.endswith(".py")):
         initial_extensions.append("cogs." + filename[:-3])
-
 if __name__ == "__main__":
     for extension in initial_extensions:
         client.load_extension(extension)
