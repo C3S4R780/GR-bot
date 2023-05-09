27b20d5d9bbf2aacdc8cfd908fd51de66545ab29
Cesar Santos <C3S4R780@gmail.com>
2023-05-09 14:20:40 -0300

    [Add] League of Legends champion and build randomizer

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -1,10 +1,20 @@
-import nextcord
-from nextcord import Interaction, SlashOption
-from nextcord.ext import commands
-from asyncio import sleep
 import os
 import random
+import requests
+import urllib.request
+import textwrap
+import nextcord
+from nextcord import Interaction, SlashOption, Message
+from nextcord.ext import commands
+from asyncio import sleep
+from PIL import Image, ImageDraw, ImageFilter, ImageEnhance, ImageFont
 
+def load_img(url, filename):
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', 'Chrome')]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(url, f"./imgs/{filename}")
+    return f'./imgs/{filename}'
 
 class Misc(commands.Cog):
     def __init__(self, client):
@@ -61,5 +71,148 @@ class Misc(commands.Cog):
         # Send the specified message, as the bot, to the current channel
         return await channel.send(msg)
 
+    @nextcord.slash_command(name="roleta_lol", description="Te desafio a fazer uma build extremamente idiota (ou não) com um campeão, itens e runas aleatorias")
+    async def roleta_lol(self, interaction: Interaction):
+
+                                    ###############################################
+                                    ##    TODO: Add random summoner's spells     ##
+                                    ###############################################
+        
+        response = await interaction.send(content="Carregando...")
+        icon_size = 125
+        rune_size = 65
+        stat_size = 35
+        version = requests.get("https://ddragon.leagueoflegends.com/realms/br.json").json()['v']
+        champs = requests.get(f"https://ddragon.leagueoflegends.com/cdn/{version}/data/pt_BR/champion.json").json()['data']
+        champ_name, champ_data = random.choice(list(champs.items()))
+        
+        splash = Image.open(load_img(
+            f"https://ddragon.leagueoflegends.com/cdn/img/champion/centered/{champ_data.get('id')}_0.jpg",
+            f"{champ_data.get('id')}_0.jpg"
+        ))
+        
+        items = requests.get(f"https://ddragon.leagueoflegends.com/cdn/{version}/data/pt_BR/item.json").json()['data']
+        item_icon_base_url = f"https://ddragon.leagueoflegends.com/cdn/{version}/img/item/"
+        item_list = [
+            item for item in items
+            if items[item].get('requiredAlly') is None and
+            items[item].get('requiredChampion') is None and
+            items[item].get('inStore') is None and
+            not "Consumable" in items[item].get('tags') and
+            not "Trinket" in items[item].get('tags') and
+            not "Lane" in items[item].get('tags') and
+            not "Jungle" in items[item].get('tags')
+        ]
+        mythics = [
+            item for item in item_list
+            if items[item].get('description').find("Mythic") != -1
+        ]
+        legendaries = [
+            item for item in item_list
+            if items[item].get('into') is None and
+            "Boots" not in items[item].get('tags') and
+            items[item].get('description').find("Mythic") == -1
+        ]
+        boots = [
+            item for item in item_list
+            if items[item].get('into') is None and
+            "Boots" in items[item].get('tags')
+        ]
+        
+        mythic = random.choice(mythics)
+        mythic_icon_url = f"{item_icon_base_url}{items[mythic].get('image').get('full')}"
+        with Image.open(load_img(
+            mythic_icon_url,
+            items[mythic].get('image').get('full')
+        )) as mythic_icon:
+            mythic_icon = mythic_icon.resize((icon_size, icon_size), Image.Resampling.LANCZOS)
+            splash.paste(mythic_icon)
+
+        row = 0
+        column = 1
+        for i in range(1, 5):
+            legendary = random.choice(legendaries)
+            legendary_icon_name = items[legendary].get('image').get('full')
+            with Image.open(load_img(
+                f"{item_icon_base_url}{legendary_icon_name}",
+                legendary_icon_name
+            )) as legendary_icon:
+                if i == 3: 
+                    row += 1
+                    column = 0
+                legendary_icon = legendary_icon.resize((icon_size, icon_size), Image.Resampling.LANCZOS)
+                splash.paste(legendary_icon, (icon_size*column, icon_size*row))
+                column += 1
+
+        boot = random.choice(boots)
+        boot_icon_name = items[boot].get('image').get('full')
+        with Image.open(load_img(
+            f"{item_icon_base_url}{boot_icon_name}",
+            boot_icon_name
+        )) as boot_icon:
+            boot_icon = boot_icon.resize((icon_size, icon_size), Image.Resampling.LANCZOS)
+            splash.paste(boot_icon, (icon_size*2, icon_size*row))
+
+        rune_pages = requests.get(f"https://ddragon.leagueoflegends.com/cdn/{version}/data/pt_BR/runesReforged.json").json()
+        rune_icon_base_url = "https://ddragon.leagueoflegends.com/cdn/img/"
+        primary_page = random.choice(rune_pages)
+        secondary_page = random.choice(rune_pages)
+
+        if primary_page.get('name') == secondary_page.get('name'):
+            secondary_page = random.choice(rune_pages)
+
+        main_rune = random.choice(primary_page.get('slots')[0].get('runes'))
+        with Image.open(load_img(
+            f"{rune_icon_base_url}{main_rune.get('icon')}",
+            main_rune.get('icon').split("/")[-1]
+        )) as main_rune_icon:
+            main_rune_icon = main_rune_icon.resize((icon_size, icon_size), Image.Resampling.LANCZOS)
+            splash.paste(main_rune_icon, (1035, 0))
+
+        column = 1000
+        for i in range(1, 4):
+            rune = random.choice(primary_page.get('slots')[i].get('runes'))
+            with Image.open(load_img(
+                f"{rune_icon_base_url}{rune.get('icon')}",
+                rune.get('icon').split("/")[-1]
+            )) as rune_icon:
+                rune_icon = rune_icon.resize((rune_size, rune_size), Image.Resampling.LANCZOS)
+                splash.paste(rune_icon, (column, 125))
+                column += rune_size
+
+        column = 1035
+        for i in range(2):
+            x = random.randint(1, 3)
+            rune = random.choice(secondary_page.get('slots')[x].get('runes'))
+            with Image.open(load_img(
+                f"{rune_icon_base_url}{rune.get('icon')}",
+                rune.get('icon').split("/")[-1]
+            )) as rune_icon:
+                rune_icon = rune_icon.resize((rune_size, rune_size), Image.Resampling.LANCZOS)
+                splash.paste(rune_icon, (column, 190))
+                column += rune_size
+
+        stat_mods = {
+            0: ["AdaptiveForceIcon", "AttackSpeedIcon", "CDRScalingIcon"],
+            1: ["AdaptiveForceIcon", "ArmorIcon", "MagicResIcon.MagicResist_Fix"],
+            2: ["HealthScalingIcon", "ArmorIcon", "MagicResIcon.MagicResist_Fix"]
+        }
+
+        column = 1050
+        for i in range(3):
+            with Image.open(load_img(
+                f"{rune_icon_base_url}perk-images/StatMods/StatMods{random.choice(stat_mods[i])}.png",
+                rune.get('icon').split("/")[-1]
+            )) as stat_icon:
+                stat_icon = stat_icon.resize((stat_size, stat_size), Image.Resampling.LANCZOS)
+                splash.paste(stat_icon, (column, 255))
+                column += stat_size
+
+        splash.save(f"./imgs/build.png")
+
+        await response.edit(content=f"> **{champ_name}**", file=nextcord.File("./imgs/build.png"))
+
+        for file in os.scandir('./imgs'):
+            os.remove(file.path)
 def setup(client):
     client.add_cog(Misc(client))
